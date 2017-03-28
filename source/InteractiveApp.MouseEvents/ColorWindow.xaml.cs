2b0aa009a90422bd45a57df70bb141c8cf6abf59
DenysPelikh <optimal.frozi@gmail.com>
2017-03-28 05:01:17 +0300

    Added Circles create logic, Color Window, some bug fix

---
+++ source/InteractiveApp.MouseEvents/ColorWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace InteractiveApp.MouseEvents
+{
+    public partial class ColorWindow : Window
+    {
+        private readonly Random _random;
+
+        private Brush _rectangleBrush;
+
+        public ColorWindow()
+        {
+            InitializeComponent();
+
+            _random = new Random();
+        }
+
+        private void RectanglesStackPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            CreateRectangles(2);
+        }
+
+        private void CreateRectangles(int number)
+        {
+            var rectangleHeigh = (int)RectanglesStackPanel.ActualWidth / number;
+
+            var rectangleColor = Color.FromRgb((byte)_random.Next(0, 255), (byte)_random.Next(0, 255), (byte)_random.Next(0, 255));
+
+            for (var i = 0; i < number; i++)
+            {
+                var coef = 1 - i * (1F / number);
+
+                var rectangle = new Rectangle
+                {
+                    Height = rectangleHeigh,
+                    Fill = new SolidColorBrush(Color.Multiply(rectangleColor, coef))
+                };
+
+                rectangle.MouseEnter += Rectangle_MouseEnter;
+                rectangle.MouseLeave += Rectangle_MouseLeave;
+
+                RectanglesStackPanel.Children.Add(rectangle);
+            }
+        }
+
+        private void Rectangle_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var rectangle = sender as Rectangle;
+
+            if (rectangle != null)
+            {
+                _rectangleBrush = rectangle.Fill.CloneCurrentValue();
+                rectangle.Fill = Brushes.Orange;
+            }
+        }
+
+        private void Rectangle_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var rectangle = sender as Rectangle;
+            if (rectangle != null) rectangle.Fill = _rectangleBrush;
+        }
+    }
+}
--- source/InteractiveApp.MouseEvents/MainWindow.xaml.cs
+++ source/InteractiveApp.MouseEvents/MainWindow.xaml.cs
@@ -1,12 +1,103 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace InteractiveApp.MouseEvents
 {
     public partial class MainWindow : Window
     {
+        private readonly Random _random;
+        private bool _isAttached;
+        private FrameworkElement _attachedElement;
+        private Point _clickPoint;
+        private ColorWindow _colorWindow;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _random = new Random();
+
+            CircleContainCanvas.MouseMove += CircleContainCanvasOnMouseMove;
+
+            Loaded += (sender, args) =>
+            {
+                CreateEllipse();
+                CreateEllipse();
+                CreateEllipse();
+            };
+        }
+
+        private void CircleContainCanvasOnMouseMove(object sender, MouseEventArgs mouseEventArgs)
+        {
+            if (_isAttached && _attachedElement != null)
+            {
+                Canvas.SetLeft(_attachedElement, mouseEventArgs.GetPosition(CircleContainCanvas).X - _clickPoint.X);
+                Canvas.SetTop(_attachedElement, mouseEventArgs.GetPosition(CircleContainCanvas).Y - _clickPoint.Y);
+            }
+        }
+
+        private void CreateEllipse()
+        {
+            var ellipse = new Ellipse
+            {
+                Height = 100,
+                Width = 100,
+                Fill = new SolidColorBrush(Colors.PaleGreen)
+            };
+
+            ellipse.MouseLeftButtonDown += EllipseOnMouseLeftButtonDown;
+            ellipse.MouseLeftButtonUp += EllipseOnMouseLeftButtonUp;
+
+            Canvas.SetLeft(ellipse, _random.Next(0, (int)((CircleContainCanvas.ActualWidth + 50) - ellipse.Width)));
+            Canvas.SetLeft(ellipse, _random.Next(0, (int)((CircleContainCanvas.ActualWidth + 50) - ellipse.Height)));
+
+            ellipse.MouseLeave += Ellipse_MouseLeave;
+            ellipse.MouseEnter += Ellipse_MouseEnter;
+
+            CircleContainCanvas.Children.Add(ellipse);
+        }
+
+        private void Ellipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var ellipse = sender as Ellipse;
+            if (ellipse != null) ellipse.Fill = Brushes.Orange;
+        }
+
+        private void Ellipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            _isAttached = false;
+            var ellipse = sender as Ellipse;
+            if (ellipse != null) ellipse.Fill = Brushes.PaleGreen;
+        }
+
+        private void EllipseOnMouseLeftButtonUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            _isAttached = false;
+        }
+
+        private void EllipseOnMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            _clickPoint = mouseButtonEventArgs.GetPosition(sender as FrameworkElement);
+            _attachedElement = sender as FrameworkElement;
+
+            _isAttached = true;
+        }
+
+        private void NewWindow_Button_Click(object sender, RoutedEventArgs e)
+        {
+            _colorWindow = new ColorWindow
+            {
+                Visibility = Visibility.Visible
+            };          
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            _colorWindow?.Close();
         }
     }
 }
